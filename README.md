37567721a9636a0e08b1bbec21cb8a7a283aa52c
Sam Thorogood <sam.thorogood@gmail.com>
2019-01-10 11:42:16 +1100

    flask zero app

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 This needs a virtualenv in Python 3.7.
+
+```bash
+virtualenv bot  # setup
+source bot/bin/activate
+```
+
--- server.py
+++ server.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Butt'
+  return 'Hello, World!'
 
-app.run()
+if __name__ == '__main__':
+  app.run()

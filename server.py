c43db4178c2168f5b16a17dc5911e836c006b4c2
Sam Thorogood <sam.thorogood@gmail.com>
2019-01-10 12:30:18 +1100

    alexa endpoint

--- server.py
+++ server.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
+
+@app.route('/alexa', methods=['POST', 'GET'])
+def alexa():
+  return jsonify({
+    'version': '0.1',
+    'response': {
+      'outputSpeech': {
+        'type': 'PlainText',
+        'text': 'Hello, welcome to my shrug',
+      },
+    },
+  })
 
 if __name__ == '__main__':
   app.run(debug=True)

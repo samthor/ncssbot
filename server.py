00f3ceb56838ae3a6816182ae7733b5ba9e36c73
Sam Thorogood <sam.thorogood@gmail.com>
2019-01-10 13:35:59 +1100

    basic more Alexa handling

--- server.py
+++ server.py
@@ -10,12 +10,36 @@ def hello_world():
 
 @app.route('/alexa', methods=['POST', 'GET'])
 def alexa():
+  data = request.get_json()
+
+  request_type = data['request']['type']
+  if request_type == 'LaunchRequest':
+    jsonify({
+      'version': '0.1',
+      'response': {
+        'outputSpeech': {
+          'type': 'PlainText',
+          'text': 'Hello, welcome to my shrug',
+        },
+      },
+    })
+
+  if request_type != 'IntentRequest':
+    raise Exception(f'unhandled request type {request_type}')
+
+  query = data['request']['intent']['slots']['query']['value']
+  print(f'handling query: `{query}')
+  if 'boat' in query:
+    response_text = 'I hear you like boats'
+  else:
+    response_text = 'Say something nautical, not ' + query
+
   return jsonify({
     'version': '0.1',
     'response': {
       'outputSpeech': {
         'type': 'PlainText',
-        'text': 'Hello, welcome to my shrug',
+        'text': response_text,
       },
     },
   })
